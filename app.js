d5e652c31a55eef328a87a2502ce6a8dbe7fb1c8
santino sanacore <santinosanacore@gmail.com>
2023-05-04 21:02:21 -0300

    segunda preentrega cargada

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let i
+/*PRIMERA PREENTREGA
+
 let camiseta = prompt("Ingrese si quiere la camiseta de Boca o de Argentina")
 
 for(let i = 0; i <= 10; i++) {
@@ -7,4 +8,47 @@ for(let i = 0; i <= 10; i++) {
     } else {
             console.log("Ingresar Boca o Argentina, con sus respectivas mayusculas y minusculas.")
         }
+} */
+
+const carrito = [];
+
+const productos = [
+    { nombre: 'CAMISETA BOCA TITULAR', precio: 21999 },
+    { nombre: 'CAMISETA BOCA SUPLENTE', precio: 21999 },
+    { nombre: 'CAMISETA ARGENTINA ICON', precio: 29999 },
+    { nombre: 'CAMISETA ARGENTINA SUPLENTE', precio: 21999 },
+    { nombre: 'CAMISETA REAL MADRID TITULAR', precio: 21999 },
+    { nombre: 'CAMISETA REAL MADRID SUPLENTE', precio: 16999 },
+    { nombre: 'SHORT ARGENTINA TITULAR', precio: 12999 },
+    { nombre: 'SHORT BOCA TITULAR', precio: 12999 },
+    { nombre: 'SHORT REAL MADRID TITULAR', precio: 12999 }
+];
+
+let seguirCompra = "SI";
+
+do{
+let nombreProducto = prompt("Que producto desea agregar al carrito? Indicar prenda, equipo y si es titular o suplente. EJ: Camiseta Argentina titular").toUpperCase();
+
+const productoEncontrado = productos.find(producto => producto.nombre === nombreProducto)
+
+function agregarAlCarrito(producto){
+    carrito.push(producto)
+    console.log(carrito)
 }
+
+if (productoEncontrado != undefined){
+    agregarAlCarrito(productoEncontrado);
+} else {
+    alert("El producto no se encuentra en la lista de productos");
+}
+
+prompt("Desea seguir comprando?. Responda Si o No").toUpperCase();
+
+} while(seguirCompra === "SI")
+
+if(seguirCompra ){
+    const total = carrito.reduce((accum, producto) => accum + producto.precio, 0)
+    console.log(`La suma total de tu carrito es de $ ${total}`)
+}
+
+
